df8a4a04684cbd8bcb9b5f6b19c42012de69cae3
yeni023 <yeeun3011@naver.com>
2025-09-01 13:51:36 +0900

    feat: 메인 페이지 프로필 영역 및 공통 Card 배치

--- src/pages/MainPage/Home.tsx
+++ src/pages/MainPage/Home.tsx
@@ -1,11 +1,22 @@
-import { CircleUserRound } from 'lucide-react';
+import { CircleUserRound, Bell } from 'lucide-react';
 import styles from './Home.module.css';
+
+import Card from '@/components/common/Card/Card';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <CircleUserRound className={styles.icon} />
-      <span className={styles.nickname}>사용자 닉네임</span>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.profile}>
+          <CircleUserRound className={styles.icon} />
+          <span className={styles.nickname}>USERNAME</span>
+        </div>
+        <Bell className={styles.bell} />
+      </div>
+      <Card>
+        <h3>8월 사용 금액</h3>
+        <p>1,031,000원</p>
+      </Card>
     </div>
   );
 }
