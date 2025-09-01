3b80cdff0e0923b83e0e66b626ad1924847af52d
yeni023 <yeeun3011@naver.com>
2025-09-01 16:33:26 +0900

    feat: 여행지 예시 카드(파리, 도쿄, 시드니) 추가

--- src/pages/MainPage/Home.tsx
+++ src/pages/MainPage/Home.tsx
@@ -17,12 +17,28 @@ export default function Home() {
       <AssetCard />
       <UsageCard />
       <TripCard
+        destination="Sydney, Australia"
+        countryCode="AU"
+        period="2026.01.10 - 2026.01.20"
+        thumbnailUrl="https://images.unsplash.com/photo-1528072164453-f4e8ef0d475a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        progressPercent={0}
+        onClickDetail={() => console.log('시드니 상세보기 클릭')}
+      />
+      <TripCard
         destination="Paris, France"
         countryCode="FR"
         period="2025.12.24 - 2025.12.31"
         thumbnailUrl="https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=640&auto=format&fit=crop"
-        progressPercent={100}
+        progressPercent={60}
         onClickDetail={() => console.log('상세보기 클릭')}
+      />
+      <TripCard
+        destination="Tokyo, Japan"
+        countryCode="JP"
+        period="2025.10.05 - 2025.10.12"
+        thumbnailUrl="https://images.unsplash.com/photo-1503899036084-c55cdd92da26?ixlib=rb-4.0.3&auto=format&fit=crop&w=640&q=80"
+        progressPercent={100}
+        onClickDetail={() => console.log('도쿄 상세보기 클릭')}
       />
     </div>
   );
