84b9a00cb2d364b09afc919a24e028536dd8370b
yeni023 <yeeun3011@naver.com>
2025-09-02 19:37:37 +0900

    docs: App.tsx 라우트 기능별 섹션 주석 정리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/MainPage/Home';
 import About from '@/pages/StartPlan/About';
+import DetailPage from '@/pages/DetailPage/DetailPage';
 import Splash from '@/pages/SplashPage/Splash';
 import Login from '@/pages/LoginPage/Login';
 
@@ -8,10 +9,22 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Auth (첫 시작) */}
         <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
+
+        {/* Home */}
         <Route path="/home" element={<Home />} />
+
+        {/* Plan (설문/시작 플로우) */}
         <Route path="/about" element={<About />} />
+
+        {/* Trip (상세) */}
+        <Route path="/trip/:tripId" element={<DetailPage />} />
+
+        {/* TODO: 404 
+        <Route path="*" element={<NotFound />} /> 
+        */}
       </Routes>
     </Router>
   );
