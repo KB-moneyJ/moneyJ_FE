ebfc6ba457b67fbde800641d29e2e63a9ecb38f7
merona0531 <moonpower0531@naver.com>
2025-09-06 21:28:36 +0900

    fix : 다음, 이전 버튼 스타일

--- src/pages/StartPlan/PlanStyle.ts
+++ src/pages/StartPlan/PlanStyle.ts
@@ -17,7 +17,24 @@ export const ContentsWrapper = styled.div`
     margin-top: 120px;
     height: 650px;
 `;
-
+export const NextBtn = styled.button`
+    width: 88px;
+    height: 45px;
+    border: none;
+    color: white;
+    font-size: 15px;
+    border-radius: 8px;
+    background-color: #5B00B2;
+`
+export const PrevBtn = styled.button`
+    width: 88px;
+    height: 45px;
+    border: none;
+    color: white;
+    font-size: 15px;
+    border-radius: 8px;
+    background-color: #525252;
+`
 
 export const ProgressBar = styled.div`
   width: 320px;
